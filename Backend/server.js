cb07f8ce76e7c133abad981ddcc604b1a819c862
MJephat <mukundijephat22@gmail.com>
2025-02-10 19:53:04 +0300

    deployment setup done

--- Backend/server.js
+++ Backend/server.js
@@ -8,17 +8,22 @@ import connectionRoutes from "./routes/connection.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 connectDB();
 
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
-}));
+})
+);
+}
 
 app.use(express.json({limit: "5mb"})); // parse json request bodies
 app.use(cookieParser());
@@ -28,6 +33,15 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications",notificationRoutes);
 app.use("/api/v1/connections",connectionRoutes);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/Frontend/dist")))
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"))
+    })
+}
 
 
 app.listen(PORT, ()=>{
